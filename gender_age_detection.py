daa1acbdedeb3c0fb7786e054e3e2912e39024da
sampathkore4444 <sampathkore4444@gmail.com>
2024-09-15 19:29:43 +0700

    Update gender_age_detection.py

--- gender_age_detection.py
+++ gender_age_detection.py
@@ -71,7 +71,7 @@ def classify_image(image):
         return fallback_label, confidence
 
 # Streamlit UI
-st.title("Bio Classification App powered by AI")
+st.title("Bio Classify AI App")
 st.write("Upload an image to analyze the gender and age group, or get general information.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
